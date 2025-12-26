c45522d9f395c271ccce47356fcfe36d2b65d0c0
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-12-26 16:29:27 +0700

    refactored code and added description to key mappings

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -4,17 +4,26 @@ return {
         "kevinhwang91/promise-async"
     },
     config = function()
+        local ufo = require("ufo")
         -- open and close all
-        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+        vim.keymap.set(
+            "n", "zR", ufo.openAllFolds,
+            { desc = "Open all folds." }
+        )
+        vim.keymap.set(
+            "n", "zM", ufo.closeAllFolds,
+            { desc = "Close all folds." }
+        )
         -- peek a fold
-        vim.keymap.set('n', 'zK', function()
-            local winid = require('ufo').peekFoldedLinesUnderCursor()
-            if not winid then
-                vim.lsp.buf.hover()
-            end
-        end)
+        vim.keymap.set(
+            "n", "zK",
+            function()
+                local winid = ufo.peekFoldedLinesUnderCursor()
+                if not winid then vim.lsp.buf.hover() end
+            end,
+            { desc = "Peek into the current fold." }    
+        )
 
-        require('ufo').setup()
+        require("ufo").setup()
     end
 }
