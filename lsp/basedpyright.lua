f0e9f6b1f5d6c607f162e452715463c1bc3279c7
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-12-27 16:26:08 +0700

    updated basedpyright config

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,17 +1,52 @@
+-- type checking config
+local diagnostic_overrides = {
+    reportUnusedClass = "hint",
+    reportUnusedFunction = "hint",
+    reportUnusedVariable = "hint",
+
+    reportGeneralTypeIssues = false,
+    reportPropertyTypeMismatch = false,
+    reportFunctionMemberAccess = "hint",
+    reportUnusedImport = "hint",
+
+    reportArgumentType = false,
+    reportAssertTypeFailure = false,
+    reportAttributeAccessIssue = "hint",
+    reportInvalidTypeArguments = false,
+    reportImplicitOverride = false,
+
+    reportUnknownParameterType = false,
+    reportUnknownArgumentType = false,
+    reportUnknownLambdaType = false,
+    reportUnknownVariableType = false,
+    reportUnknownMemberType = false,
+
+    reportMissingParameterType = false,
+    reportMissingTypeArgument = false,
+
+    -- basedpyright exclusive
+    reportAny = false,
+    reportExplicitAny = false,
+    reportUnusedParameter = "hint",
+    reportUnannotatedClassAttribute = false,
+} 
 return {
     settings = {
         basedpyright = {
-            venvPath = ".",
+            python = { 
+                venvPath = ".",
+                venv = ".venv"
+            },
             analysis = {
                 diagnosticMode = "openFilesOnly",
-                typeCheckingMode = "off",
                 autoSearchPaths = true,
-                -- configFilePath = "pyrightconfig.json",
                 inlayHints = {
                     callArgumentNames = true,
                     variableTypes = false,
                     parameterTypes = false
-                }
+                },
+
+                diagnosticSeverityOverrides = diagnostic_overrides
             }
         }
     }
