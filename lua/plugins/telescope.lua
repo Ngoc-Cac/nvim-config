cfff301c415d408bc18116e135cfcddc0fec5974
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-12-28 13:42:38 +0700

    added key map for searching notifications

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -24,25 +24,26 @@ end
 local default_options = {
     winblend = 5,
     border = true,
-    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' }
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
 }
 
 return {
     "nvim-telescope/telescope.nvim", tag = "v0.2.0",
     dependencies = { "nvim-lua/plenary.nvim" },
     init = function()
-        vim.api.nvim_create_autocmd({ 'User', 'ColorScheme' }, {
+        vim.api.nvim_create_autocmd({ "User", "ColorScheme" }, {
             callback = set_telescope_hl
         })
     end,
     config = function()
         require("telescope").setup({defaults = default_options})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = 'Telescope list marks' })
-        vim.keymap.set('n', '<leader>fc', builtin.colorscheme, { desc = 'Telescope list colorscheme' })
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set("n", "<leader>fb", builtin.current_buffer_fuzzy_find, { desc = "Telescope buffers" })
+        vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Telescope list marks" })
+        vim.keymap.set("n", "<leader>fc", builtin.colorscheme, { desc = "Telescope list colorscheme" })
+        vim.keymap.set("n", "<leader>fn", ":Telescope notify<CR>", { desc = "Telescope list notify" })
     end
 }
