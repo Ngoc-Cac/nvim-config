5bc79b0eda9b7fe159096bdf16b7d2deb2f74d9e
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-12-30 11:27:51 +0700

    refactored some configuration for which-key

--- lua/plugins/small-plugs.lua
+++ lua/plugins/small-plugs.lua
@@ -15,18 +15,7 @@ return {
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
-        opts = {
-            preset = "helix",
-        },
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
+        opts = { preset = "helix" },
     },
     -- commenting code
     {
