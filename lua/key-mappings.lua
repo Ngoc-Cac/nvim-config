639a5a5b7ec3f7672f57962a0e08886937a68348
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-01 11:25:58 +0700

    forgot to stage

--- lua/key-mappings.lua
+++ lua/key-mappings.lua
@@ -2,6 +2,10 @@ local keyset = vim.keymap.set
 
 -- escape key
 keyset("i", "jj", "<Esc>")
+keyset("c", "jj", "<C-c>")
+
+-- quick command mode
+keyset("n", "<leader>;", ":", { desc = "Enter command-line mode" })
 
 -- quick undo and redo in insert mode
 keyset("i", "<C-z>", "<C-o>u")
@@ -25,7 +29,3 @@ keyset("n", "<M-j>", ":m+<CR>==")
 keyset("n", "<M-k>", ":m-2<CR>==")
 keyset("v", "<M-j>", ":m'>+1<CR>gv=gv")
 keyset("v", "<M-k>", ":m'<-2<CR>gv=gv")
-
--- better page down and pge up
-keyset("n", "<C-d>", "<C-d>zz")
-keyset("n", "<C-u>", "<C-u>zz")
