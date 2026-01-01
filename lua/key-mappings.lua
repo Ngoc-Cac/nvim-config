a06d421a24e98eaa90d11c44dc2259b8977db81d
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-01 13:33:05 +0700

    refactored code

--- lua/key-mappings.lua
+++ lua/key-mappings.lua
@@ -19,9 +19,7 @@ keyset("i", "<C-s>", "<C-o>:w<CR>")
 -- if at end of line: space -> normal -> "_cb
 -- else move right once, enter normal mode and replace backwards
 keyset("i", "<C-h>", function()
-    if vim.fn.col(".") == vim.fn.col("$") then
-        return ' <Esc>"_cb'
-    else return '<Right><Esc>"_cb' end
+    return (vim.fn.col(".") == vim.fn.col("$") and " " or "<Right>") .. '<Esc>"_cb'
 end, { expr = true })
 
 -- alt + motion to move line up and down
