f04ef0c0734b309a312b5aa7a69bb3f2f615bf1a
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-06 11:21:10 +0700

    updated vim-airline config: - added vim-fugitive as a dependency so it can recognizes the plugin.   Sometimes, there is a timing issue in the plugin loading that causes vim-airline to not recognize that vim-fugitive is installed. - updated the b section of the statusline into the c section.

--- lua/plugins/ui/vim-airline.lua
+++ lua/plugins/ui/vim-airline.lua
@@ -1,8 +1,8 @@
 local function init_airline()
     local create = vim.fn["airline#section#create"]
     vim.g.airline_section_a = create({'mode', ' ', 'branch'})
-    vim.g.airline_section_b = create({'hunks', '| ', 'file'})
-    vim.g.airline_section_c = nil
+    vim.g.airline_section_c = create({'hunks', '<', 'file'})
+    vim.g.airline_section_b = nil
 end
 vim.api.nvim_create_autocmd("User", {
     pattern = "AirlineAfterInit",
@@ -12,7 +12,9 @@ vim.api.nvim_create_autocmd("User", {
 return {
     {
         "vim-airline/vim-airline",
-        dependencies = { "vim-airline/vim-airline-themes" },
+        dependencies = {
+            "tpope/vim-fugitive", "vim-airline/vim-airline-themes"
+        },
         init = function()
             vim.g.airline_theme = "molokai"
             vim.g.airline_powerline_fonts = 1
