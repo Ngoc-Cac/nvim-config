2ee5293d5c0b1289262f580455d3283897f2b8b5
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-15 08:31:56 +0700

    minor bug fix to keep the key-mappings description.

--- lua/plugins/edit_utils/split-join.lua
+++ lua/plugins/edit_utils/split-join.lua
@@ -2,13 +2,17 @@
 return {
     "Wansmer/treesj",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
-    keys = { "<leader>sm", "<leader>sj", "<leader>ss" },
+    keys = {
+        { "<leader>sm", desc = "Toggle treesj" },
+        { "<leader>sj", desc = "Join code block with treesj" },
+        { "<leader>ss", desc = "Split code block with treesj" },
+    },
     config = function()
         local sj = require("treesj")
         sj.setup({ use_default_keymaps = false })
 
-        vim.keymap.set("n", "<leader>sm", sj.toggle, { desc = "Toggle treesj" })
-        vim.keymap.set("n", "<leader>sj", sj.join, { desc = "Join code block with treesj" })
-        vim.keymap.set("n", "<leader>ss", sj.toggle, { desc = "Split code block with treesj" })
+        vim.keymap.set("n", "<leader>sm", sj.toggle)
+        vim.keymap.set("n", "<leader>sj", sj.join)
+        vim.keymap.set("n", "<leader>ss", sj.toggle)
     end,
 }
