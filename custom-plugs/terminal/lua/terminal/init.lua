f41cf32910cb470d96bcd835b8fa7bdd81676c68
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-15 08:21:56 +0700

    refactored terminal plugin: - moved the core logic to core submodule. - the init logic now only changes the config. - Removed the default keybinding and expose a ToggleTerm command   instead. - Moved the default keymapping to be set by lazy.nvim

--- custom-plugs/terminal/lua/terminal/init.lua
+++ custom-plugs/terminal/lua/terminal/init.lua
@@ -1,78 +1,22 @@
--- Functions for toggling terminal
--- keep track of the win and buffer
-local utils = require("terminal.utils")
-local config = {
-    split = {
-        width_ratio = 0.3,
-        height_ratio = 0.3,
-        width = nil,
-        height = nil
-    },
-    float = {
-        float_ratio = 0.8,
-        width = nil,
-        height = nil
-    },
-}
-local term = { buf = nil, win = nil, split_kind = "vertical" }
+local terminal_core = require("terminal.core")
 
-local function close_win_if_exists()
-    local is_open = utils.is_valid_win(term.win)
-    if is_open then
-        vim.api.nvim_win_close(term.win, true)
-        term.win = nil
-    end
-    return is_open
-end
+local function cmdline_complete(ArgLead, _, _)
+    local directions = { 'float', 'above', 'below', 'left', 'right' }
 
-local function get_win_wh(split_kind)
-    local width = nil
-    local height = nil
-    if split_kind == "float" then
-        local ui = vim.api.nvim_list_uis()[1]
-        width = config.float.width or math.floor(ui.width * config.float.float_ratio)
-        height = config.float.height or math.floor(ui.height * config.float.float_ratio)
-    else
-        width = config.split.width or math.floor(vim.o.columns * config.split.width_ratio)
-        height = config.split.height or math.floor(vim.o.columns * config.split.height_ratio)
-    end
-    return width, height
-end
+    if ArgLead == '' then return directions end
 
-local function toggle_term(split)
-    split = split or "right" -- vertical split by default
-
-    -- only returns if not switching view
-    if close_win_if_exists() and term.split_kind == split then return end
-
-    -- create buf if no buffer
-    if not utils.is_valid_buf(term.buf) then term.buf = utils.create_term_buf() end
-
-    local w, h = get_win_wh(split)
-    term.win = utils.create_win(term.buf, { split = split, width = w, height = h })
-    term.split_kind = split
-
-    vim.keymap.set(
-        { "n", "i", "t" },
-        "<esc>q", function()
-            vim.cmd.stopinsert()
-            toggle_term(term.split_kind)
-        end,
-        { buffer = term.buf, desc = "Toggle current terminal off" }
-    )
+    return vim.tbl_filter(function(val)
+        return vim.startswith(val, ArgLead)
+    end, directions)
 end
 
 local function setup(opts)
     opts = opts or {}
-    config = vim.tbl_deep_extend("force", config, opts)
+    terminal_core.config = vim.tbl_deep_extend("force", terminal_core.config, opts)
 end
 
-vim.keymap.set(
-    "n", "<LocalLeader>tv", toggle_term,
-    { desc = "Toggle terminal in split view." }
+vim.api.nvim_create_user_command(
+    "ToggleTerm", function(opts) terminal_core.toggle_term(opts.args) end,
+    { nargs = 1, complete = cmdline_complete }
 )
-vim.keymap.set(
-    "n", "<LocalLeader>tf", function() toggle_term("float") end,
-    { desc = "Toggle terminal in float window." }
-)
-return { setup = setup, toggle_term = toggle_term }
+return { setup = setup, toggle_term = terminal_core.toggle_term }
--- lua/plugins/custom-plugs.lua
+++ lua/plugins/custom-plugs.lua
@@ -4,11 +4,30 @@ return {
     {
         dir = config_path .. "/custom-plugs/terminal",
         name = "toggle-terminal",
-        opts = {}
+        cmd = "ToggleTerm",
+        keys = {
+            {
+                "<localleader>tv", ":ToggleTerm right<cr>",
+                desc = "Toggle terminal in split view.",
+            },
+            {
+                "<localleader>tf", ":ToggleTerm float<cr>",
+                desc = "Toggle terminal in float window.",
+            },
+        },
+        opts = {
+            split = { width_ratio = 0.45 },
+            float = { float_ratio = 0.85 }
+        }
     },
     {
         dir = config_path .. "/custom-plugs/buffers",
         name = "buffers",
+        cmd = "R",
+        keys = {
+            { "<localleader>bd", desc = "Delete the current buffer." },
+            { "<localleader>bb", desc = "Delete all buffers except the current one." }
+        },
         opts = {}
     }
 }
