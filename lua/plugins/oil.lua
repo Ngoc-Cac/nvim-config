743c427b892518c7ae99878c317959a4a2a1c98d
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-16 09:40:07 +0700

    added oil.nvim for filesystem navigation and modification

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,28 @@
+return {
+    'stevearc/oil.nvim',
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = true,
+    cmd = "Oil",
+    keys = {
+        { "-", ":Oil<cr>", desc = "Open Oil" },
+        { "<leader>ol", ":topleft 30vsplit | Oil .<cr>", desc = "Open Oil to the left" },
+        { "<leader>ot", ":tab Oil .<cr>", desc = "Open Oil in new tab" },
+        { "<leader>of", ":Oil --float .<cr>", desc = "Open Oil in float window"}
+    },
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+        buf_options = { bufhidden = "delete" },
+        skip_confirm_for_simple_edits = true, -- no del, at most one cp/mv and five adds
+        view_options = { show_hidden = true },
+        float = {
+            border = "rounded",
+            override = function(defaults)
+                defaults['title'] = ''
+                defaults['title_pos'] = 'center'
+                return defaults
+            end
+        },
+        confirmation = { border = "rounded" }
+    },
+}
