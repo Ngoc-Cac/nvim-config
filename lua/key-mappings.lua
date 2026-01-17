e900a380200214f8c8df49d388ff8fa10ed926e4
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-17 09:21:54 +0700

    added description to key-mappings and refactored code

--- lua/key-mappings.lua
+++ lua/key-mappings.lua
@@ -1,3 +1,9 @@
+local function backpsace_del()
+    -- if at end of line: space -> normal -> "_cb
+    -- else move right once, enter normal mode and replace backwards
+    return (vim.fn.col(".") == vim.fn.col("$") and " " or "<right>") .. '<esc>"_cb'
+end
+
 local keyset = vim.keymap.set
 
 -- escape key
@@ -5,16 +11,7 @@ keyset("i", "jj", "<esc>")
 keyset("i", "jk", "<esc>")
 keyset("i", "kj", "<esc>")
 keyset("c", "jj", "<c-c>")
-keyset(
-    "t", "<esc><esc>", "<C-\\><C-n>",
-    { desc = "Exit terminal mode." }
-)
--- not recommended because this can be
--- annoying when doing interactive git
--- vim.keymap.set(
---     "t", "jj", "<C-\\><C-n>",
---     { desc = "Exit terminal mode." }
--- )
+keyset("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Exit terminal mode." })
 
 -- quick command mode
 keyset({ "n", "v" }, ";", ":", { desc = "Enter command-line mode" })
@@ -24,20 +21,16 @@ keyset("n", "<localleader>l", ":nohl<cr>", { desc = "Quickly clear the highlight
 keyset("i", "<c-r>", "<c-o><c-r>")
 
 -- saving with ctrl-s
-keyset("n", "<c-s>", ":w<cr>")
-keyset("i", "<c-s>", "<c-o>:w<cr>")
+keyset("n", "<c-s>", ":w<cr>", { desc = ":w" })
+keyset("i", "<c-s>", "<c-o>:w<cr>", { desc = ":w" })
 
 -- ctrl-backspace in insert mode
--- if at end of line: space -> normal -> "_cb
--- else move right once, enter normal mode and replace backwards
-keyset("i", "<c-h>", function()
-    return (vim.fn.col(".") == vim.fn.col("$") and " " or "<right>") .. '<esc>"_cb'
-end, { expr = true })
+keyset("i", "<c-h>", backpsace_del, { expr = true, desc = "Delete word backwards in insert mode" })
 -- delete everything in file
-keyset("n", "X", ":%d_<cr>")
+keyset("n", "X", ":%d_<cr>", { desc = "Delete everything in the current buffer" })
 
 -- alt + motion to move line up and down
-keyset("n", "<m-j>", ":m+<cr>==")
-keyset("n", "<m-k>", ":m-2<cr>==")
-keyset("v", "J", ":m'>+1<cr>gv=gv")
-keyset("v", "K", ":m'<-2<cr>gv=gv")
+keyset("n", "<m-j>", ":m+<cr>==", { desc = "Move the current line up" })
+keyset("n", "<m-k>", ":m-2<cr>==", { desc = "Move the current line down" })
+keyset("v", "J", ":m'>+1<cr>gv=gv", { desc = "Move select block up" })
+keyset("v", "K", ":m'<-2<cr>gv=gv", { desc = "Move selected block down" })
