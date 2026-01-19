1f4ca1455fc98e9cd54b9f8e62f74dbc478f3400
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-19 07:24:00 +0700

    added multi-cursor plugin

--- lua/plugins/edit_utils/init.lua
+++ lua/plugins/edit_utils/init.lua
@@ -7,5 +7,27 @@ return {
             "gcc", { "gc", mode = { "n", "x", "o" } },
             { "<C-_>", "<C-o>gcc", mode = "i", remap = true, desc = "Comment selected code." },
         }
+    },
+    {
+        "jake-stewart/multicursor.nvim",
+        branch = "1.0",
+        keys = { "<m-K>", "<m-J>", "<c-n>", "<c-q>" },
+        config = function()
+            local mc = require("multicursor-nvim")
+            mc.setup()
+            local set = vim.keymap.set
+
+            set({"n", "x"}, "<m-K>", function() mc.lineAddCursor(-1) end)
+            set({"n", "x"}, "<m-J>", function() mc.lineAddCursor(1) end)
+            set({"n", "x"}, "<c-n>", function() mc.matchAddCursor(1) end)
+
+            -- only works in multi-cursor mode
+            mc.addKeymapLayer(function(layerSet)
+                layerSet({"n", "x"}, "<up>", mc.prevCursor)
+                layerSet({"n", "x"}, "<down>", mc.nextCursor)
+                layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
+                layerSet("n", "<esc>", function() mc.clearCursors() end)
+            end)
+        end
     }
 }
