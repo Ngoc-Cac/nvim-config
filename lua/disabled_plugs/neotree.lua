a8ffada2aedc14726c0b4b0de77a5af0df4b3a92
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-25 10:12:24 +0700

    Update indentation for neotree plugin
    This plugin is unused, but update indentation just to be consistent with
    other lua files

--- lua/disabled_plugs/neotree.lua
+++ lua/disabled_plugs/neotree.lua
@@ -1,41 +1,41 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons",
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    enabled=false,
-    keys = {
-        {
-            "<C-b>", ":Neotree show toggle<cr>",
-            mode = { "n", "i" }, desc = "Toggle Neotree."
-        }
-    },
-    ---@module 'neo-tree'
-    ---@type neotree.Config
-    opts = {
-        close_if_last_window = true,
-        add_blank_line_at_top = true,
-        auto_clean_after_session_restore = true,
-        window = {width = 30},
-        buffers = {show_unloaded = true},
-        filesystem = {
-            -- hidden files
-            filtered_items = {visible = true}
-        },
-        default_component_configs = {
-            git_status = { symbols = {
-                added = "A",
-                modified = "M",
-                untracked = "U",
-                ignored = "",
-                unstaged = "",
-                staged = "A",
-                conflict = "C"
-            }}
-        }
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  enabled=false,
+  keys = {
+    {
+      "<C-b>", ":Neotree show toggle<cr>",
+      mode = { "n", "i" }, desc = "Toggle Neotree."
     }
+  },
+  ---@module 'neo-tree'
+  ---@type neotree.Config
+  opts = {
+    close_if_last_window = true,
+    add_blank_line_at_top = true,
+    auto_clean_after_session_restore = true,
+    window = {width = 30},
+    buffers = {show_unloaded = true},
+    filesystem = {
+      -- hidden files
+      filtered_items = {visible = true}
+    },
+    default_component_configs = {
+      git_status = { symbols = {
+        added = "A",
+        modified = "M",
+        untracked = "U",
+        ignored = "",
+        unstaged = "",
+        staged = "A",
+        conflict = "C"
+      }}
+    }
+  }
 }
