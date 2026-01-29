b690ac0ea7497be33f36cd09079f8021d8adb949
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-29 07:21:17 +0700

    Update the `buf_del` function to use the buf history
    Make the `buf_del` function to move down the tracked history as the
    buffers are deleted.
    
    This is basically the original move to alternate buffer but now the
    alternate buffer goes beyong just the next/prev buffer.

--- custom-plugs/buffer-utils/lua/buffer-utils/init.lua
+++ custom-plugs/buffer-utils/lua/buffer-utils/init.lua
@@ -1,31 +1,22 @@
+local hist_tracker = require("buffer-utils.history-tracker")
+
 local function buf_del()
   local cur_buf = vim.api.nvim_get_current_buf()
   local cur_win = vim.api.nvim_get_current_win()
 
-  local next_win = nil
-  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-    if win ~= cur_win then
-      next_win = cur_win
-      break
-    end
-  end
-
-  if next_win then
-    vim.api.nvim_set_current_win(next_win)
-  else
-    local alt_buf = vim.fn.bufnr("#")
-    local is_valid = vim.api.nvim_buf_is_loaded(alt_buf)
-
-    if alt_buf == -1 or not is_valid then
+  local history = hist_tracker.buffer_history[cur_win]
+  if #history == 0 then
+    local win_closed = pcall(vim.api.nvim_win_close, cur_win, false)
+    if not win_closed then
       vim.notify(
         "No previous buffer to switch to!",
         vim.log.levels.INFO,
         { title = "No previous buffer!", timeout = 3000 }
       )
-      return
-    elseif is_valid and alt_buf ~= cur_buf then
-      vim.api.nvim_set_current_buf(alt_buf)
     end
+  else
+    vim.api.nvim_set_current_buf(history[#history])
+    table.remove(history)
   end
 
   vim.api.nvim_buf_delete(cur_buf, { force = false })
