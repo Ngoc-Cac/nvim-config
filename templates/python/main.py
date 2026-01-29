1c259b39e5a7e41b9a872aaf636c85796b5a98b5
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-29 11:34:09 +0700

    Update main python template file
    Add a bunch of errors and hint diagnostics to the template file to test
    for LSP features.

--- templates/python/main.py
+++ templates/python/main.py
@@ -1,7 +1,8 @@
 import operator as opt
-import numpy as np
+import numpy as np  # should not raise missing import 
+import not_a_module as module
 
-from mathparse import MathEvaluator 
+from mathparse import MathEvaluator, does_not_exist
 from mathparse.operators import (
     MathConstant,
     BinaryOperator,
@@ -11,12 +12,28 @@ from mathparse.operators import (
 
 
 def factorial(num: float) -> int:
+    """
+    This is a docstring that describes this function.
+
+    Code block to explains some math stuff, I guess::
+
+        1 + 1 = 2
+
+    Args:
+        num (float): This describes the parameter and what it does.
+
+    Returns:
+        int: This describes what the function returns.
+    """
     if not num.is_integer():
-        raise TypeError()
-    else: num = int(num)
+        raise ValueError("Just an error")
+    else:
+        num = int(num)
+
     result = 1
     for i in range(2, num + 1):
         result *= i
+         SyntaxError("This isn't correct!")
     return result
 
 FACTORIAL = UnaryOperator('!', factorial, 'left')
@@ -36,16 +53,35 @@ COT = SVF('cot', lambda x: 1 / np.tan(x))
 evaluator = MathEvaluator(
     [
         ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION,
-        EXPONENTIATION, FACTORIAL,
+        EXPONENTIATION FACTORIAL,  # missing comma
     ],
     [SIN, COS, TAN, COT],
     [MathConstant('pi', np.pi), MathConstant('e', np.e)],
     MULTIPLICATION.token
 )
 
-def evaluate(input: str) -> tuple[int | float]:
+def evaluate(input: str) -> int | float:
+    """
+    This is a docstring which explains what this function do.
+    But this uses a different format.
+
+    ## A section about something specific 
+    Some markdown syntax too:
+        - something
+        - another point
+        - something else
+
+    :param str input: And this explains what this parameter do/represent. 
+    :return: This describes what the function return
+    :rtype: int or float
+    """
+    if not input:
+        might_be_unbound_var = "maybe just maybe"
+    print(unbound_var, might_be_unbound_var) 
+
     result = evaluator.postfit_evaluate(evaluator.parse(input.lower()))
     return int(result) if result.is_integer() else result
+    print("Unreachable code")
 
 
 # This is a comment
@@ -55,6 +91,30 @@ np_array = np.random.randint(0, 1, size=(100, 5))
 results = np_array ** 2 - np.cos(np_array)
 
 expr = "5!*sin(pi /2) + 2^2 - 5cos(tan(2))"
+expr_result = evaluate(expr)  # should register as int | float 
+evaluator.cannot_access
+evaluator.cannot_assign = 2  # this is because MathEvaluator registers attributes using __slots__
+
 print("Evaluating expression")
 print(evaluator.parse(expr.lower()))
-print(f"Got: {expr} = {evaluate(expr)}")
+print(f"Got: {expr} = {expr_results}")
+
+
+def decorate(func):
+    def nested_func(*args):
+        print("Wrapped!")
+        func(*args)
+    return nested_func
+
+def decorator_w_args(doesnt_really_matter=None):
+    if doesnt_really_matter:
+        return lambda _: None
+    return decorate
+
+@decorate
+def decorated_func(name):
+    print(f"Hello {name}")
+
+@decorator_w_args(doesnt_really_matter=0, not_an_args=5)
+def this_is_wraped(unused):
+    print("I'm out of ideas lol")
