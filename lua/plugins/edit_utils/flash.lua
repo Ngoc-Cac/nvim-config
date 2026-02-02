206f703b719c2487515a78470d812c2d56b9e5b7
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-02 09:18:50 +0700

    Update `flash.nvim` configuration
    Re-enable the flash jump feature and override the regular jump feature
    
    Add some configuration to update the label and make the jump feature
    behave jsut like the regular jump feature

--- lua/plugins/edit_utils/flash.lua
+++ lua/plugins/edit_utils/flash.lua
@@ -5,11 +5,25 @@ return {
     { "f", mode = { "n", "v" } }, { "F", mode = { "n", "v" } },
     { "t", mode = { "n", "v" } }, { "T", mode = { "n", "v" } },
 
-    -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "/", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
     -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
-  opts = {},
+  opts = {
+    jump = {
+      history = true,
+      register = true
+    },
+    label = {
+      current = false,
+      after = false,
+      before = true,
+      rainbow = {
+        enabled = true,
+        shade = 9
+      }
+    }
+  },
 }
