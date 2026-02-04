6b8cadc69087dfe7aeec614d0bd1eba173daad1e
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-04 08:36:52 +0700

    Reformat config code for `ufo.nvim`

--- lua/plugins/ui/ufo.lua
+++ lua/plugins/ui/ufo.lua
@@ -10,16 +10,10 @@ return {
     ufo.setup(opts)
 
     -- open and close all
-    vim.keymap.set(
-      "n", "zR", ufo.openAllFolds, { desc = "Open all folds." }
-    )
-    vim.keymap.set(
-      "n", "zM", ufo.closeAllFolds, { desc = "Close all folds." }
-    )
+    vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds." })
+    vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds." })
     -- peek a fold
-    vim.keymap.set(
-      "n", "zK",
-      function()
+    vim.keymap.set("n", "zK", function()
         local winid = ufo.peekFoldedLinesUnderCursor()
         if not winid then vim.lsp.buf.hover() end
       end,
