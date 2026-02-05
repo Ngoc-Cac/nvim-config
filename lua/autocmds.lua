bbd523d7b6f98678bfae0d5da43a3921cac4d55e
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-05 19:34:47 +0700

    Add more files to the treesitter autocmd
    Automatically enable treesitter on more filetypes that I current edit.

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,6 +6,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = {
     "lua", "python", "markdown",
     "json", "yaml", "toml",
+    "html", "css", "javascript"
   },
   callback = function() vim.treesitter.start() end,
 })
