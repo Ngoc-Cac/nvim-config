7ce349740b1f57538207f8a3cdb76df48f1a4181
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-07 15:12:07 +0700

    Migrate to `auto-session` for session manager
    This plugin has mroe feature that I need, like session naming

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,18 @@
+return {
+	"rmagatti/auto-session",
+	lazy = false,
+  keys = {
+    { "<localleader>ss", "<cmd>AutoSession search<CR>", desc = "Session search" },
+    { "<localleader>sd", "<cmd>AutoSession deletePicker<CR>", desc = "Session delete" },
+    { "<localleader>sc", "<cmd>AutoSession save ", desc = "Session save" },
+  },
+	---@module "auto-session"
+	---@type AutoSession.Config
+	opts = {
+		suppressed_dirs = { "~/", "~/Downloads", "/" },
+    auto_create = false,
+    auto_save = true,
+    auto_restore_last_session = true,
+    bypass_save_filetypes = { "terminal" },
+	},
+}
