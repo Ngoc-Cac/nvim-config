af16c764a706c8ea7073b14ed0469ac4553ae5ce
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-22 09:20:32 +0700

    Fix minor bug for `<c-h>` mapping
    The ctrl-backspace delete utility will not delete the extra space that
    was added in the case the cursor is at the end of the line.
    
    The new strategy now is to use `dbs` to delete backwards and then
    substitute the last undeleted character.
    
    This should now work no matter where the cursor is at.
    
    Also add a new case where the cursor is at the beginning of the line, in
    which only the newline character is deleted.

--- custom-plugs/keymaps-extend/lua/keymaps-extend/ctrl-backspace.lua
+++ custom-plugs/keymaps-extend/lua/keymaps-extend/ctrl-backspace.lua
@@ -1,7 +1,6 @@
 local function backspace_del()
-  -- if at end of line: space -> normal -> "_cb
-  -- else move right once, enter normal mode and replace backwards
-  return (vim.fn.col(".") == vim.fn.col("$") and " " or "<right>") .. '<esc>"_cb'
+  -- if at the beginning then just delete the newline
+  return vim.fn.col(".") == 1 and "<bs>" or '<esc>"_dbs'
 end
 
 return { backspace_del = backspace_del }
