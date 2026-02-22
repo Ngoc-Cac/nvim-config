6eadfce0418f58c4a457e2e408c85de731773ac2
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-22 10:04:21 +0700

    Update the `<c-h>` mapping to deal with indentation
    When everything before the cursor is whitespaces, the utility function
    should delete them and stop at the beginning of the line.

--- custom-plugs/keymaps-extend/lua/keymaps-extend/ctrl-backspace.lua
+++ custom-plugs/keymaps-extend/lua/keymaps-extend/ctrl-backspace.lua
@@ -1,6 +1,13 @@
 local function backspace_del()
-  -- if at the beginning then just delete the newline
-  return vim.fn.col(".") == 1 and "<bs>" or '<esc>"_dbs'
+  local col = vim.fn.col(".")
+  local before_cur = vim.api.nvim_get_current_line():sub(1, col - 1)
+
+  if col == 1 then
+    return "<bs>" -- if at the beginning then just delete the newline
+  else
+    -- if before cursor is all spaces then delete them
+    return '<esc>"_d' .. (before_cur:match("^%s+$") and "0s" or "bs")
+  end
 end
 
 return { backspace_del = backspace_del }
