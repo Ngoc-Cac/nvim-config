a73587d5ba2c77b7a7cba4f3136aa58080e2cdab
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-23 09:12:03 +0700

    Reformat code

--- init.lua
+++ init.lua
@@ -38,7 +38,13 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system({
+    "git", "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath,
+  })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -64,7 +70,10 @@ require("lazy").setup({
   },
 })
 
-vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { desc = "Open the lazy.nvim menu" })
+vim.keymap.set(
+  "n", "<leader>l", ":Lazy<CR>",
+  { desc = "Open the lazy.nvim menu" }
+)
 
 -- vim.cmd.colorscheme("monokai-pro")
 vim.cmd.colorscheme("jellybeans")
