23ad21c2e0ba78315b341eff630cf00b7a18e406
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-25 20:56:21 +0700

    Add key-mappings for markdown preview
    This is only for markdown files, of course

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,3 +2,9 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.wrap = true
+
+vim.keymap.set(
+  "n", "<localleader>m",
+  ":MarkdownPreviewToggle<cr>",
+  { desc = "Toggle md preview", buffer = true }
+)
