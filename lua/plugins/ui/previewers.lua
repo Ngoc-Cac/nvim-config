1cdab0b4d3aaacf7b75d2dfadfa68766ff1a31ac
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-25 20:53:53 +0700

    Fix the lazy-loading issue for `markdown-preview.nvim`
    When this plugin is lazy-loaded on the commands, it will raise an error
    saying that the trigger command doesn't exist, most likely because of a
    missing BufEnter event.
    
    To deal with this, we have to use a deferred execution of the command
    and manually lazy-load this plugin.
    
    from #585

--- lua/plugins/ui/previewers.lua
+++ lua/plugins/ui/previewers.lua
@@ -1,9 +1,34 @@
+local md_cmds = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" }
+local function register_lazy_cmd()
+  for _, cmd in pairs(md_cmds) do
+    -- referenced from https://github.com/dopey/nvim/commit/c3e068b847e8ac5bbea425940b0b8de8cd9707a2
+    -- issue: https://github.com/iamcco/markdown-preview.nvim/issues/585
+    vim.api.nvim_create_user_command(
+      cmd,
+      function()
+        vim.cmd.delcommand(cmd)
+        require("lazy").load(
+          { plugins = { "markdown-preview.nvim" } }
+        )
+        -- commands appear only after BufEnter
+        vim.api.nvim_exec_autocmds("BufEnter", {})
+        vim.cmd(cmd)
+      end,
+      {}
+    )
+  end
+end
+
 return {
   { -- a web-based version, which is nice to test stuff out, but requires npm 
     "iamcco/markdown-preview.nvim",
     build = "cd app && npm install && git restore .", -- npm modifies lock for some reason 
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    lazy = true,
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+
+      register_lazy_cmd()
+    end,
   },
   -- color previewer
   {
