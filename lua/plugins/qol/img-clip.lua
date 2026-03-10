6b61391bcef08972180d4caa5e23c1669bb87b35
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-03-10 19:28:01 +0700

    Update config for `img-clip.nvim`
    - Add key mappings for the paste command.
    
    - Change the directory to `figures` for tex files.

--- lua/plugins/qol/img-clip.lua
+++ lua/plugins/qol/img-clip.lua
@@ -1,9 +1,15 @@
 return {
 	"HakonHarnes/img-clip.nvim",
 	cmd = "PasteImage",
+  keys = {
+    { "<localleader>p", ":PasteImage<cr>", desc = "Paste image from clipboard"}
+  },
 	opts = {
 		default = {
 			show_dir_path_in_prompt = true,
 		},
+    filetypes = {
+      tex = { dir_path = "figures" }
+    }
 	},
 }
