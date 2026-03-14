a1182e16c0011ed85afc7423a35b2b0e6f10bdc9
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-03-14 11:05:19 +0700

    Update the priorties once again
    The original capture groups won't capture everything, so I'll just
    gonna copy the original query file and add some prioties to them.

--- after/queries/comment/highlights.scm
+++ after/queries/comment/highlights.scm
@@ -1,17 +1,60 @@
 ; extends
 
-("text" @comment.todo
+((tag
+  (name) @comment.todo @nospell
+  ("(" @punctuation.bracket
+    (user) @constant
+    ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
   (#any-of? @comment.todo "TODO" "WIP")
   (#set! "priority" 126))
 
-("text" @comment.note
-  (#any-of? @comment.note "NOTE")
+("text" @comment.todo @nospell
+  (#any-of? @comment.todo "TODO" "WIP")
   (#set! "priority" 126))
 
-("text" @comment.warning
-  (#any-of? @comment.warning "WARN" "WARNING")
+((tag
+  (name) @comment.note @nospell
+  ("(" @punctuation.bracket
+    (user) @constant
+    ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#any-of? @comment.note "NOTE" "XXX" "INFO" "DOCS" "PERF" "TEST")
   (#set! "priority" 126))
 
-("text" @comment.error
-  (#any-of? @comment.error "ERROR" "BUG" "FIXME")
+("text" @comment.note @nospell
+  (#any-of? @comment.note "NOTE" "XXX" "INFO" "DOCS" "PERF" "TEST")
   (#set! "priority" 126))
+
+((tag
+  (name) @comment.warning @nospell
+  ("(" @punctuation.bracket
+    (user) @constant
+    ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#any-of? @comment.warning "HACK" "WARNING" "WARN" "FIX")
+  (#set! "priority" 126))
+
+("text" @comment.warning @nospell
+  (#any-of? @comment.warning "HACK" "WARNING" "WARN" "FIX")
+  (#set! "priority" 126))
+
+((tag
+  (name) @comment.error @nospell
+  ("(" @punctuation.bracket
+    (user) @constant
+    ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#any-of? @comment.error "FIXME" "BUG" "ERROR")
+  (#set! "priority" 126))
+
+("text" @comment.error @nospell
+  (#any-of? @comment.error "FIXME" "BUG" "ERROR")
+  (#set! "priority" 126))
+
+; Issue number (#123)
+("text" @number
+  (#lua-match? @number "^#[0-9]+$")
+  (#set! "priority" 126))
+
+(uri) @string.special.url @nospell
