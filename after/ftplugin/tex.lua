e671afae9cd33d48ab084ff67b305c7dbce74ee5
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-04-10 21:17:58 +0700

    Add tex key-mappings for jumping text-objects
    Add some key mappings for jumping to equations and environments

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -2,10 +2,19 @@
 -- fires last. We load treesitter on `BufReadPost` and `BufNewFile`
 
 local select = require("nvim-treesitter-textobjects.select").select_textobject
+local move = require("nvim-treesitter-textobjects.move")
+
 local keyset = function(lhs, textobj, desc)
   vim.keymap.set({ "x", "o" }, lhs, function()
     select(textobj, "textobjects")
   end, { desc = desc, buffer = true })
+end
+local keyjump = function(lhs, textobj, goto_func, desc)
+  vim.keymap.set(
+    { "n", "x", "o" }, lhs,
+    function() goto_func(textobj, "textobjects") end,
+    { desc = desc }
+  )
 end
 
 keyset("a$", "@inline_formula.outer", "Inline math")
@@ -16,3 +25,13 @@ keyset("ie", "@equation.inner", "Equation")
 
 keyset("af", "@block.outer", "Environment block")
 keyset("if", "@block.inner", "Environment block")
+
+keyjump("]f", "@block.outer", move.goto_next_start, "Next environment start")
+keyjump("[f", "@block.outer", move.goto_previous_start, "Previous environment start")
+keyjump("]F", "@block.outer", move.goto_next_end, "Next environment end")
+keyjump("[F", "@block.outer", move.goto_previous_end, "Previous environment end")
+
+keyjump("]e", "@equation.outer", move.goto_next_start, "Next equation start")
+keyjump("[e", "@equation.outer", move.goto_previous_start, "Previous equation start")
+keyjump("]E", "@equation.outer", move.goto_next_end, "Next equation end")
+keyjump("[E", "@equation.outer", move.goto_previous_end, "Previous equation end")
