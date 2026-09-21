6beec3f7a9e0ea5e8f9a91b28f9f816ea9cbbf8e
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-09-21 10:14:11 +0700

    Fix minor bug with the paste action in SSH session
    Previously, the paste action also uses OSC 52 codes, which will ask to
    read the client's clipboard.
    
    In this case, client may or may not enable this and result in a hanging
    process waiting for the client to send clipboard data.
    
    THe fix is to use the unnamed register by default.

--- lua/globals.lua
+++ lua/globals.lua
@@ -20,17 +20,13 @@ if vim.fn.has("win32") == 1 then
     cache_enabled = 0,
   }
 elseif vim.env.SSH_TTY then
+  local function get_clipboard() return { vim.fn.getreg('"', 1, true) } end
+
   -- emit osc52 escape codes over ssh session so we can forward clipboards
   local osc52 = require('vim.ui.clipboard.osc52')
   vim.g.clipboard = {
     name = 'osc52',
-    copy = {
-      ['+'] = osc52.copy('+'),
-      ['*'] = osc52.copy('*'),
-    },
-    paste = {
-      ['+'] = osc52.paste('+'),
-      ['*'] = osc52.paste('*'),
-    },
+    copy = { ['+'] = osc52.copy('+'), ['*'] = osc52.copy('*') },
+    paste = { ["+"] = get_clipboard, ["*"] = get_clipboard },
   }
 end
